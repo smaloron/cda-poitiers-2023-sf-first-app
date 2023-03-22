46c9d47105b91ce349c8317418236b30c39421bc
seb <contact@smaloron.eu>
2023-03-22 11:50:55 +0100

    Validation des formulaires

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\AuthorRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
 #[ORM\Table(name: 'authors')]
@@ -19,9 +20,18 @@ class Author
     private ?string $firstName = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\NotBlank(message: 'Le nom ne peut être vide')]
+    #[Assert\Length(min: 3, max: 30,
+        minMessage: 'Le nom ne peut comporter moins de {{ limit }} caractères',
+        maxMessage: 'Le nom ne peur comporter plus de {{ limit }} caractères'
+    )
+    ]
     private ?string $lastName = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Assert\Length(max: 144,
+        maxMessage: 'Votre texte ne peut comporter plus de {{ limit }} caractères')
+    ]
     private ?string $bio = null;
 
     public function getId(): ?int
