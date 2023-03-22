a3ef4ca15c6e431276207353294b2f62600f2249
seb <contact@smaloron.eu>
2023-03-22 14:32:04 +0100

    Fixtures pour l'entité Book

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -16,29 +16,35 @@ class AppFixtures extends Fixture
         $author->setFirstName('Pablo')
             ->setLastName('Neruda');
         $manager->persist($author);
+        $this->addReference('author_1', $author);
 
         $author = new Author();
         $author->setFirstName('Jorge Luis')
             ->setLastName('Borges');
         $manager->persist($author);
+        $this->addReference('author_2', $author);
 
         $author = new Author();
         $author->setFirstName('Emily')
             ->setLastName('Dickinson');
         $manager->persist($author);
+        $this->addReference('author_3', $author);
 
         // création des éditeurs
         $publisher = new Publisher();
         $publisher->setName('PUF')->setCity('Paris');
         $manager->persist($publisher);
+        $this->addReference('publisher_1', $publisher);
 
         $publisher = new Publisher();
         $publisher->setName('Harper & Collins')->setCity('New York');
         $manager->persist($publisher);
+        $this->addReference('publisher_2', $publisher);
 
         $publisher = new Publisher();
         $publisher->setName('Grasset')->setCity('Paris');
         $manager->persist($publisher);
+        $this->addReference('publisher_3', $publisher);
 
 
         $manager->flush();
