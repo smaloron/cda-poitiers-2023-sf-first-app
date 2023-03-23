370f85e97d0626185543640607a51e4a605cc1fd
seb <contact@smaloron.eu>
2023-03-23 14:36:00 +0100

    Amélioration des fixtures avec Foundry

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/DataFixtures/PersonFixtures.php
+++ src/DataFixtures/PersonFixtures.php
@@ -6,6 +6,9 @@ use App\Entity\Address;
 use App\Entity\Skill;
 use App\Entity\Student;
 use App\Entity\Teacher;
+use App\Factory\AddressFactory;
+use App\Factory\SkillFactory;
+use App\Factory\TeacherFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -14,6 +17,37 @@ class PersonFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
+        SkillFactory::createOne(['skillName' => 'PHP']);
+        SkillFactory::createOne(['skillName' => 'Java']);
+        SkillFactory::createOne(['skillName' => 'React']);
+        SkillFactory::createOne(['skillName' => 'Android']);
+        SkillFactory::createOne(['skillName' => 'SQL']);
+        SkillFactory::createOne(['skillName' => 'Python']);
+        SkillFactory::createOne(['skillName' => 'C++']);
+        SkillFactory::createOne(['skillName' => 'C#']);
+        SkillFactory::createOne(['skillName' => 'Ruby']);
+        SkillFactory::createOne(['skillName' => 'Go']);
+        SkillFactory::createOne(['skillName' => 'Scala']);
+        SkillFactory::createOne(['skillName' => 'Haskell']);
+
+        AddressFactory::createMany(300);
+
+        TeacherFactory::createOne([
+            'lastName' => 'Koudelka',
+            'address' => AddressFactory::random()
+        ]);
+        TeacherFactory::createOne();
+        TeacherFactory::createMany(
+            50,
+            function(){
+                return [
+                    'address' => AddressFactory::random(),
+                    'skills' => SkillFactory::randomRange(1, 4)
+                ];
+            }
+        );
+
+        /*
         $teacher = new Teacher();
 
         $address = (new Address())->setStreet('5 rue du bac')
@@ -42,5 +76,6 @@ class PersonFixtures extends Fixture
         $manager->persist($student);
 
         $manager->flush();
+        */
     }
 }
